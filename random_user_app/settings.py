336bf3b8ddcbf1c9c4d538bc8dbdfc45bbec1056
ТвойИмяНаGitHub <@.com>
2025-05-21 10:14:35 +0300

    settings

---
+++ random_user_app/settings.py
@@ -0,0 +1,23 @@
+from functools import lru_cache
+
+from pydantic import ConfigDict, PostgresDsn
+from sqlalchemy import create_engine
+
+
+
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    DB_DSN: PostgresDsn = "postgresql+asyncpg://postgres:postgres@localhost:5432/postgres"
+    API_URL: str = ''
+
+    model_config = ConfigDict(env_file=".env")
+
+
+@lru_cache
+def get_settings() -> Settings:
+    return Settings()
+
+
+engine = create_engine(echo=True)
